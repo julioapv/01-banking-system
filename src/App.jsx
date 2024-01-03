06b1d4eedcb0c47f4fb7764af67e73d64959e950
Julio Peña <juliopa54@gmail.com>
2024-01-03 14:22:57 -0400

    finished basic login, now working on counting attempts

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import { Login } from "./components/Login"
 function App() {
 
   return (
-    <div className="bg-gray-800 w-screen h-screen flex items-center justify-center p-10">
-      <div className="bg-blue-100 h-full">
+    <div className="bg-gray-800 w-screen h-screen flex items-center justify-center">
         <Login></Login>
-      </div>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { UserIcon, LockClosedIcon } from '@heroicons/react/24/solid'
 
 const Login = () => {
 
@@ -21,44 +22,90 @@ const Login = () => {
     const [errorMessages, setErrorMessages] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
 
-    //JSX for error message
+    //Constant for failed attempts
+    const [FailedAttempts, SetFailedAttempts] = useState(1)
+
+    //Code for handling submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        //Get data from form
+        const { uname, pass } = document.forms[0];
+
+        //Find user login information
+        const userData = fakeDB.find((user) => user.username === uname.value)
+
+        //Compare data to the DB
+        if (userData) {
+            if(userData.password !== pass.value) {
+                setErrorMessages({name: "pass", message: errors.pass})
+            } else {
+                setIsSubmitted(true)
+            }
+        } else {
+            setErrorMessages({name: "uname", message: errors.uname})
+        }
+    }
+
+
+    //JSX for error message and attempts
     const renderErrorMessage = (name) => {
-        name === errorMessages.name && (
-            <div className="">
-                {errorMessages.message}
-            </div>
+        return (
+            name === errorMessages.name && (
+                <div className="text-red-500 font-bold text-xl">
+                    {errorMessages.message}
+                </div>
+            )
         )
     }
 
-    //Code for handling submi
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    }
-
-    return (
-        <div>
+    const renderForm = (
             <form
-            onSubmit={handleSubmit}>
-                <div>
-                    <label>Username</label>
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-center items-center gap-5"
+            >
+                <div className="flex flex-col items-center mb-2">
+                    <label className="text-xl font-bold mb-2 flex items-center gap-1">
+                        Username
+                        <UserIcon className="h-6 w-6"></UserIcon>    
+                    </label>
                     <input 
                     type="text"
                     name="uname"
-                    required />
+                    required
+                    className=""
+                    />
                     {renderErrorMessage("uname")}
                 </div>
-                <div>
-                    <label>Password</label>
+
+                <div className="flex flex-col items-center mb-2">
+                    <label className="text-xl font-bold mb-2 flex items-center gap-1">
+                        Password
+                        <LockClosedIcon className="h-6 w-6" />    
+                    </label>
                     <input 
                     type="password"
                     name="pass"
                     required />
                     {renderErrorMessage("pass")}
                 </div>
-                <div>
-                    <input type="submit" />
-                </div>
+
+                <button
+                type="submit"
+                className="border rounded-lg py-2 px-3 bg-blue-500 font-bold"
+                >
+                    Log in
+                </button>
+                {FailedAttempts > 1 ? `You have ${FailedAttempts} left` : console.log(FailedAttempts)}
             </form>
+    )
+
+    return (
+        <div>
+            <div className="bg-blue-200 w-full flex flex-col gap-5 p-2 border rounded-md">
+                {isSubmitted ? null : <p className="text-5xl font-bold text-center">Sign In</p>}
+                {isSubmitted ? <div>Logged successfully</div> : renderForm}
+            </div>
         </div>
     )
 }
