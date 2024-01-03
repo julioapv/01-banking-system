448b87e5749f4fd2c03c432d479bed990808883b
Julio <juliopa54@gmail.com>
2024-01-02 23:36:09 -0400

    started working on login component and logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Login } from "./components/Login"
 function App() {
 
   return (
-    <>
-      <Login></Login>
-    </>
+    <div className="bg-gray-800 w-screen h-screen flex items-center justify-center p-10">
+      <div className="bg-blue-100 h-full">
+        <Login></Login>
+      </div>
+    </div>
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,65 @@
+import { useState } from "react"
+
 const Login = () => {
+
+    const fakeDB = [
+        {
+            username: "user1",
+            password: "pass1"
+        },
+        {
+            username: "user2",
+            password: "pass2"
+        }
+    ];
+
+    const errors = {
+        uname: "Invalid username :(",
+        pass: "Invalid password >:("
+    }
+
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    //JSX for error message
+    const renderErrorMessage = (name) => {
+        name === errorMessages.name && (
+            <div className="">
+                {errorMessages.message}
+            </div>
+        )
+    }
+
+    //Code for handling submi
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <h1>Hola Mundo</h1>
+        <div>
+            <form
+            onSubmit={handleSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input 
+                    type="text"
+                    name="uname"
+                    required />
+                    {renderErrorMessage("uname")}
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input 
+                    type="password"
+                    name="pass"
+                    required />
+                    {renderErrorMessage("pass")}
+                </div>
+                <div>
+                    <input type="submit" />
+                </div>
+            </form>
+        </div>
     )
 }
 
