47db77a45b3bf6bac234f919045e3bd0de86a599
Julio <juliopa54@gmail.com>
2024-01-03 23:50:36 -0400

    started working on bank account dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Login } from "./components/Login"
+import { DashBoard } from "./components/DashBoard";
 
 function App() {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="bg-gray-800 w-screen h-screen flex items-center justify-center">
         <Login isSubmitted={isSubmitted} setIsSubmitted={setIsSubmitted}></Login>
-        
+        { isSubmitted ? <DashBoard /> : null}
     </div>
   )
 }
---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,9 @@
+const DashBoard = () => {
+    return (
+        <div className="bg-green-500">
+            <p className="text-5xl">Welcome back!</p>
+        </div>
+    )
+}
+
+export { DashBoard }
