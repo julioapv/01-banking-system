dcebe96d32fa07671642ffb8da6e21f1c9bb5afa
Julio Peña <juliopa54@gmail.com>
2024-01-05 19:22:12 -0400

    challenge completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from "react";
 import { Login } from "./components/Login"
 import { DashBoard } from "./components/DashBoard";
+import { EyeIcon } from '@heroicons/react/24/solid'
+
 
 function App() {
-  const [isSubmitted, setIsSubmitted] = useState(true);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isRevealed, setIsRevealed] = useState(false)
 
   return (
-    <div className="bg-gray-800 w-screen h-screen flex items-center justify-center">
+    <div className="bg-gray-800 w-screen h-screen flex flex-col items-center justify-center gap-5">
         { isSubmitted ? <DashBoard /> : <Login isSubmitted={isSubmitted} setIsSubmitted={setIsSubmitted}></Login>}
+        
+        <div>
+          <button
+          className="py-2 px-5 bg-slate-400 active:bg-slate-500 rounded-full font-bold "
+          onClick={() => setIsRevealed(!isRevealed)}
+          >
+            <p className="flex gap-1">
+              Reveal users
+              <EyeIcon className="h-6 w-6" />
+            </p>
+          </button>
+
+          <div className={!isRevealed ? "hidden" : "block text-white"}>
+            <li>user1 / pass1</li>
+            <li>user2 / pass2</li>
+          </div>
+        </div>
+
+        <footer className="text-white text-3xl">
+          Challenge made by <a className="text-cyan-500" target="_blank" href="https://github.com/julioapv" rel="noreferrer">Julio Peña </a>
+        </footer>
     </div>
   )
 }
--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -3,40 +3,74 @@ import { ArrowDownOnSquareStackIcon, ArrowUpOnSquareIcon, ArrowsUpDownIcon } fro
 
 const DashBoard = () => {
     const [balance, setBalance] = useState(2000);
-    const [inputValue, setInputValue] = useState("")
-    //Deposit function
 
-    const [depositAmout, setDepositAmount] = useState(0)
+    //Balance for secondary accounts
+    const [balanceA, setBalanceA] = useState(1500)
+    const [balanceB, setBalanceB] = useState(10)
+    
+    
+    //Deposit function
+    const [deposit, setDeposit] = useState(0)
 
     const depositMoney = () => {
-        setDepositAmount(depositAmout + parseFloat(inputValue));
-        setInputValue("")
-        console.log(depositAmout);
+        setBalance(prevBalance => prevBalance + parseFloat(deposit))
     }
 
-    const handleDepositData = (e) => {
-        setInputValue(e.target.value)
-    }
+    //Withdraw function
+    const [withdraw, setWithdraw] = useState(0)
 
-    
     const withdrawMoney = () => {
-        console.log('withdraw');
+        if (parseFloat(withdraw) <= balance) {
+            setBalance(prevBalance => prevBalance - parseFloat(withdraw))
+        } else {
+            alert("Insufficient balance for withdrawal")
+        }
     }
 
-    const transferMoney = () => {
-        console.log('transfer');
+    //Transfer function
+    const [transfer, setTransfer] = useState(0)
+
+    const transferMoneyA = () => {
+        if (parseFloat(transfer) <= balance) {
+            setBalance(prevBalance => prevBalance - parseFloat(transfer))
+            setBalanceA(prevBalance => prevBalance + parseFloat(transfer))
+
+        } else {
+            alert("Insufficient balance for transfer")
+        }
+    }
+    
+    const transferMoneyB = () => {
+        if (parseFloat(transfer) <= balance) {
+            setBalance(prevBalance => prevBalance - parseFloat(transfer))
+            setBalanceB(prevBalance => prevBalance + parseFloat(transfer))
+
+        } else {
+            alert("Insufficient balance for transfer")
+        }
     }
 
     return (
-        <div className="bg-blue-100">
+        <div className="bg-blue-100 p-5 rounded-md shadow-lg">
             <div>
-                <p className="text-5xl">Welcome back!</p>
-                <p className="text-2xl">Balance: <span className="font-bold text-green-700">${balance}</span></p>
+                <p className="text-5xl mb-5">Welcome back!</p>
+                <p className="text-3xl">Balance: <span className="font-bold text-green-700">${balance}</span></p>
+            </div>
+
+            <div className="flex gap-2 mt-2 mb-5">
+                <div className="bg-emerald-500 rounded-full flex items-center justify-center py-2 px-5 gap-1 shadow-md">
+                    <p className="font-bold">Account A:</p>
+                    <p >${balanceA}</p>
+                </div>
+                <div className="bg-rose-500 rounded-full flex items-center justify-center py-2 px-5 gap-1 shadow-md">
+                    <p className="font-bold">Account B:</p>
+                    <p>${balanceB}</p>
+                </div>
             </div>
 
             <div className="flex flex-col justify-center gap-2">
                 <p className="text-4xl font-bold text-center">Actions</p>
-                <div className="grid grid-rows-1 grid-cols-3 gap-3">
+                <div className="flex justify-between gap-3">
                     <div className="bg-violet-200 p-2 flex flex-col items-center gap-3">
                         <p className="text-2xl flex items-center justify-center gap-1">
                             Deposit 
@@ -49,8 +83,7 @@ const DashBoard = () => {
                             step={0.1} 
                             min={0} 
                             className="outline-none border focus:border-blue-400"
-                            value={inputValue}
-                            onChange={handleDepositData}
+                            onChange={(e) => setDeposit(e.target.value)}
                             />
                         </div>
                         <button 
@@ -72,13 +105,14 @@ const DashBoard = () => {
                             step={0.1} 
                             min={0} 
                             className="outline-none border focus:border-blue-400"
+                            onChange={(e) => setWithdraw(e.target.value)}
                             />
                         </div>
                         <button 
                         className="text-xl text-white bg-blue-500 border rounded-lg py-2 px-5 active:bg-blue-700"
                         onClick={withdrawMoney}
                         >
-                            Deposit
+                            Withdraw
                         </button>
                     </div>
                     <div className="bg-pink-200 p-2 flex flex-col items-center gap-3">
@@ -93,14 +127,24 @@ const DashBoard = () => {
                             step={0.1} 
                             min={0} 
                             className="outline-none border focus:border-blue-400"
+                            onChange={(e) => setTransfer(e.target.value)}
                             />
                         </div>
+                        <div>
                         <button 
-                        className="text-xl text-white bg-blue-500 border rounded-lg py-2 px-5 active:bg-blue-700"
-                        onClick={transferMoney}
+                        className="text-xl text-white bg-emerald-500 border rounded-lg py-2 px-5 active:bg-emerald-700"
+                        onClick={transferMoneyA}
                         >
-                            Deposit
+                            Transfer A
                         </button>
+                        <button 
+                        className="text-xl text-white bg-rose-500 border rounded-lg py-2 px-5 active:bg-rose-700"
+                        onClick={transferMoneyB}
+                        >
+                            Transfer B
+                        </button>
+                        </div>
+
                     </div>
                     </div>
             </div>
